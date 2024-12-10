8db69c7e79d87608a06a86d9faa464fc4c1b5f18
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-10 21:58:09 +0800

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import ReactDom from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 
-ReactDom.createRoot(document.getElementById("root")).render(
-  <Provider>
-    <PersistGate>
-      <App />
-    </PersistGate>
-  </Provider>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
