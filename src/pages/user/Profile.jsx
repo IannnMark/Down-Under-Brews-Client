9ff37af4afd2ce79d0ee84d9962f6182d2d4a7e0
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-14 20:51:33 +0800

    profile ui

---
+++ src/pages/user/Profile.jsx
@@ -0,0 +1,87 @@
+import { useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+export default function Profile() {
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+  const [formData, setFormData] = useState({});
+  const [selectedImage, setSelectedImage] = useState(null);
+  const dispatch = useDispatch();
+  const fileInputRef = useRef(null);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedImage(file);
+      setFormData({ ...formData, avatar: file });
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          hidden
+          accept="image/*"
+          ref={fileInputRef}
+          onChange={handleImageChange}
+        />
+
+        <img
+          src={
+            selectedImage
+              ? URL.createObjectURL(selectedImage)
+              : currentUser.avatar || "default-avatar.jpg"
+          }
+          alt="Profile"
+          className="w-24 h-24 rounded-full mt-2 cursor-pointer"
+          onClick={() => fileInputRef.current.click()}
+        />
+
+        <input
+          type="text"
+          placeholder="username"
+          defaultValue={currentUser.username}
+          id="username"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          defaultValue={currentUser.email}
+          id="email"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="*****"
+          id="password"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-black text-white rounded-lg p-3 text-xl hover:opacity-95 disabled:opacity-80 mt-5"
+        >
+          {loading ? "Loading..." : "Update"}
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">SignOut</span>
+      </div>
+      <p className="text-red-700">{error ? error : ""}</p>
+    </div>
+  );
+}
