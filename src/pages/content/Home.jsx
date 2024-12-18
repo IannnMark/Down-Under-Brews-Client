bace44c2324f94c0e3feeed324ef0115134cf351
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-18 20:34:27 +0800

    homepage

--- src/pages/content/Home.jsx
+++ src/pages/content/Home.jsx
@@ -1,4 +1,7 @@
 import heroImage from "../../../images/hero_image.png";
+import aboutImage1 from "../../../images/about_img_1.png";
+import aboutImage2 from "../../../images/about_img_2.png";
+import aboutImage3 from "../../../images/about_img_3.png";
 export default function Home() {
   return (
     <div>
@@ -12,7 +15,7 @@ export default function Home() {
           Discover the art of sustainable coffee crafted with passion and a
           touch of Aussie spirit.
         </p>
-        <button className="bg-red-900 text-white uppercase px-3 py-2 rounded-md mt-6 font-semibold">
+        <button className="bg-red-900 text-white uppercase px-3 py-2 rounded-md mt-6 font-semibold hover:scale-105 transition duration-300">
           Shop coffee blends
         </button>
         <div
@@ -23,7 +26,97 @@ export default function Home() {
             backgroundPosition: "bottom",
           }}
         ></div>
-        <div></div>
+      </div>
+      <div className="flex flex-row items-center gap-6 p-16 text-orange-300 bg-red-950 mt-[-60px]">
+        <div className="flex flex-row items-center gap-6 p-16 mt-[-60px] mx-64">
+          <div className="flex flex-col items-center">
+            <div className="inline-flex items-center font-bold">
+              <h1 className="text-6xl">15</h1>
+              <span className="text-lg ml-1">+</span>
+            </div>
+            <p className="text-sm text-center">Years of roasting experience</p>
+          </div>
+          <div className="h-24 border-l-2 border-orange-300 mx-10"></div>
+          <div className="flex flex-col items-center">
+            <div className="inline-flex items-center font-bold">
+              <h1 className="text-6xl">50 </h1>
+              <span className="text-lg ml-1">+</span>
+            </div>
+            <p className="text-sm text-center">Unique coffee blends crafted</p>
+          </div>
+          <div className="h-24 border-l-2 border-orange-300 mx-10"></div>
+          <div className="flex flex-col items-center">
+            <div className="inline-flex items-center font-bold">
+              <h1 className="text-6xl">1,200</h1>
+              <span className="text-lg ml-1">+</span>
+            </div>
+            <p className="text-sm text-center">Satisfied customers served</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-row p-16 text-orange-300 bg-red-950 mt-[-110px]">
+        <div className="flex-1 text-left">
+          <h1 className="text-[43px] font-semibold leading-normal">
+            <span className="inline">From Bean to Brew - Meet Down</span>
+            <br /> <span className="block">Under Brews</span>
+          </h1>
+          <p className="text-[16px] font-normal mt-10">
+            Born in Melbourne, Down Under Brews embodies the essence of
+            Australian coffee culture. We take pride in roasting coffee that's
+            not just a drink, but a connection to the craft.
+          </p>
+          <br />
+          <p className="text-[16px] font-normal">
+            Our blends are sustainably sourced, carefully crafted, and inspired
+            by the natural beauty of the land down under.
+          </p>
+          <br />
+          <p className="text-[16px] font-normal">
+            We strive to support local communities and promote eco-friendly
+            practices. Every cup reflects our dedication to quality,
+            sustainability, and the unique culture that defines Australian
+            coffee.
+          </p>
+
+          <div className="mt-10">
+            <button className="text-red-950 uppercase text-[16px] font-semibold bg-orange-300 rounded-lg px-3 py-2">
+              Our Story
+            </button>
+            <button className="text-orange-300 uppercase text-[16px] font-semibold ml-5 border border-gray-300 rounded-lg px-3 py-2">
+              Meet the Roasters
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-center gap-4 mt-28">
+          <div
+            className="w-[315px] h-[354px] rounded-md"
+            style={{
+              backgroundImage: `url(${aboutImage1})`,
+              backgroundSize: "cover",
+              backgroundPosition: "bottom",
+            }}
+          ></div>
+          <div
+            className="w-[315px] h-[190px] rounded-md"
+            style={{
+              backgroundImage: `url(${aboutImage2})`,
+              backgroundSize: "cover",
+              backgroundPosition: "bottom",
+            }}
+          ></div>
+        </div>
+        <div className="flex flex-col gap-4 ml-5">
+          <div
+            className="w-[482px] h-[626px] rounded-md"
+            style={{
+              backgroundImage: `url(${aboutImage3})`,
+              backgroundSize: "cover",
+              backgroundPosition: "bottom",
+            }}
+          ></div>
+        </div>
       </div>
     </div>
   );
